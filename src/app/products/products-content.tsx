6767666d0c2672c8e698d230826c2956a9b35962
Gaurikas07 <gaurikas54@gmail.com>
2026-02-18 10:31:11 +0530

    Create products-content.tsx

---
+++ src/app/products/products-content.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+// other imports...
+
+export default function ProductsContent() {
+  const searchParams = useSearchParams();
+
+  // your existing logic here
+
+  return (
+    <div>
+      {/* your existing JSX */}
+    </div>
+  );
+}
