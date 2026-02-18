9c4bcf6868154f051b3bcc28c3fbc9a2731a0ac3
Gaurikas07 <gaurikas54@gmail.com>
2026-02-18 10:21:51 +0530

    Create summary-content.tsx

---
+++ src/app/order-summary/summary-content.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+
+export default function OrderSummaryContent() {
+  const searchParams = useSearchParams();
+
+  // your existing logic here
+
+  return (
+    <div>
+      {/* your existing JSX */}
+    </div>
+  );
+}
